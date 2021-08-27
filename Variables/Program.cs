8876b59f4adb141feb7de5168b9c77fc6eb70679
Christian Anderson <ca96187@gmail.com>
2021-08-27 19:29:32 -0400

    generated variables and constants

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -10,6 +10,26 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Mosh";
+            var isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
         }
     }
 }
