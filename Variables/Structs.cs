96d4de31d1044fb7e4236e8420e16161743d6b6e
Christian Anderson <ca96187@gmail.com>
2021-08-28 18:03:31 -0400

    explored struct

---
+++ Variables/Structs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    public struct Structs
+    {
+
+    }
+
+    public struct RgbColor
+    {
+        public int Red;
+        public int Green;
+        public int Blue;
+    }
+
+    /*
+     * use a struct if you want to deal with a small object structure like RgbColor above
+     * if you plan to use thousands of an object, use a struct versus a class
+     */
+
+}
