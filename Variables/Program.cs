6b7cd2a9efa2c7e995bad39016dbf46ccca5089c
Christian Anderson <ca96187@gmail.com>
2021-08-28 18:17:38 -0400

    array examples

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -10,14 +10,37 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            Person john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
-            john.Introduce();
+            //Person john = new Person();
+            //john.FirstName = "John";
+            //john.LastName = "Smith";
+            //john.Introduce();
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            //Calculator calculator = new Calculator();
+            //var result = calculator.Add(1, 2);
+            //Console.WriteLine(result);
+
+            var numbers = new int[3];
+
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Jack", "John", "Mary" };
+
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
         }
     }
 }
