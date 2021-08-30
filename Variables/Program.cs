c92a87163f43da428f6a2ed2514b927b1c0a25e3
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-29 20:01:31 -0400

    created algo to return max num from user input

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -47,20 +47,42 @@ namespace Variables
 
             //}
 
-            var random = new Random();
+            //var random = new Random();
 
-            const int passwordLength = 10;
+            //const int passwordLength = 10;
 
-            var buffer = new char[passwordLength];
+            //var buffer = new char[passwordLength];
 
-            for (int i = 0; i < passwordLength; i++)
+            //for (int i = 0; i < passwordLength; i++)
+            //{
+            //    buffer[i] = ((char)('a' + random.Next(0, 26)));
+
+            //}
+            //var password = new string(buffer);
+            //Console.WriteLine(password);
+
+            Exercise();
+        }
+
+        public static void Exercise()
+        {
+            Console.Write("Enter comma seperated numbers:");
+            var input = Console.ReadLine();
+
+            var numbers = input.Split(',');
+
+            var max = Convert.ToInt32(numbers[0]);
+
+            foreach (var str in numbers)
             {
-                buffer[i] = ((char)('a' + random.Next(0, 26)));
-                
+                var number = Convert.ToInt32(str);
+                if (number > max)
+                {
+                    max = number;
+                }
             }
-            var password = new string(buffer);
-            Console.WriteLine(password);
 
+            Console.WriteLine("Max is " + max);
         }
 
     }
